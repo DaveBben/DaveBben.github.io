704262a3aa949998d1ed55f98875275a032c10e6
David Bennett <david.a.bennett7@gmail.com>
2019-02-26 06:20:47 -0500

    first push

---
+++ js/search.js
@@ -0,0 +1,14 @@
+fetch('/lunr.json')
+  .then(response => {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error('Something went wrong on api server!');
+    }
+  })
+  .then(response => {
+    console.debug(response);
+    // ...
+  }).catch(error => {
+    console.error(error);
+  })
